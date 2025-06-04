61c49b8a2a39febfb9538ea4f979fb61902a3cca
Doh Landrine <landrinedoh22@gmail.com>
2025-06-04 04:38:21 +0100

    Fix password field name in user model and refactor authentication routes

--- models/user_model.js
+++ models/user_model.js
@@ -7,7 +7,7 @@ const userSchema = new Schema(
             type: String,
             required: true
         },
-        passwordu: {
+        password: {
             type: String,
             required: true
         }
--- routes/auth_api.js
+++ routes/auth_api.js
@@ -1,68 +1,67 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const userModel = require('../models/user_model');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/user_model");
 
 const authRouter = express.Router();
 
-authRouter.post('/create-user', async(request, response) => {
+authRouter.post("/create-user", async (request, response) => {
+  const name = request.body.name;
+  const password = request.body.password;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  userModel.findOne({ name: name }).then((user) => {
+    if (user) {
+      response.status(400).send("User already exists");
+    } else {
+      userModel
+        .create({
+          name: name,
+          password: hashedPassword,
+        })
+        .then((user) => {
+          response.send(user);
+        });
+    }
+  });
+
+  authRouter.post("/login", async (request, response) => {
     const name = request.body.name;
     const password = request.body.password;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    userModel.findOne({ name: name }).then(
-        (user) => {
-            if (user) {
-                response.status(400).send("User already exists");
-            } else {
-                userModel.create({
-                    "name": name,
-                    "password": hashedPassword
-                }).then(
-                    (user) => {
-                        response.send(user);
-                    }
-                );
-            }
+    userModel.findOne({ name: name }).then(async (user) => {
+      if (!user) {
+        response.status(400).send("User not found");
+      } else {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+          const token = jwt.sign({ id: user._id }, "secret_key", {
+            expiresIn: "1h",
+          });
+          response.json({ token: token });
+        } else {
+          response.status(400).send("Invalid password");
         }
-    );
+      }
+    });
+  });
 
-    userModel.post('login', async(request, response) => {
-        const name = request.body.name;
-        const password = request.body.password;
-
-        userModel.findOne({ name: name }).then(
-            async (user) => {
-                if (!user) {
-                    response.status(400).send("User not found");
-                } else {
-                    const isPasswordValid = await bcrypt.compare(password, user.password);
-                    if (isPasswordValid) {
-                        const token = jwt.sign({ id: user._id }, 'secret_key', { expiresIn: '1h' });
-                        response.json({ token: token });
-                    } else {
-                        response.status(400).send("Invalid password");
-                    }
-                }
-            }
-        );
+  function verifyToken(request, response, next) {
+    const token = request.headers["authorization"];
+    if (!token) {
+      return response.status(403).send("No token provided");
     }
-    );
-
-    function verifyToken(request, response, next) {
-        const token = request.headers['authorization'];
-        if (!token) {
-            return response.status(403).send("No token provided");
-        }
-        jwt.verify(token, 'secret_key', (err, decoded) => {
-            if (err) {
-                return response.status(500).send("Failed to authenticate token");
-            }
-            request.userId = decoded.id;
-            next();
-        });
-    }
-}
-)
+    jwt.verify(token, "secret_key", (err, decoded) => {
+      if (err) {
+        return response.status(500).send("Failed to authenticate token");
+      }
+      request.userId = decoded.id;
+      next();
+    });
+  }
+//   router.get("/dashboard", verifyToken, (req, res) => {
+//     res.json({ message: `Welcome ${req.user.username}!` });
+//   });
+});
 
 module.exports = authRouter;
